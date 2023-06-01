473b913a6439ed0d093a45b481ba25e59fda59e8
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-06-01 18:53:11 -0500

    Se crean los archivos de rutas y la importación dinámica de rutas

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { router } from './routes';
 
 const PORT=process.env.PORT || 8080;
 const app = express();
 app.use(cors());
+app.use(router);
 app.listen(PORT,
     ()=>console.log('Corriendo en localhost'+PORT));
---
+++ src/routes/carros.ts
@@ -0,0 +1,17 @@
+import { Router,Request,Response } from "express";
+
+const router=Router();
+/*
+get obtener datos
+post enviar datos
+put editar un registro
+delete eliminar un registro
+patch editar parcialmente un registro
+*/
+//La dirección del proyecto donde apunta
+// tiene una función, viene de los controladores
+router.get('/',(req:Request,res:Response)=>{
+    res.send({message:"Soy un carro"});
+})
+// exportar la ruta
+export {router};
